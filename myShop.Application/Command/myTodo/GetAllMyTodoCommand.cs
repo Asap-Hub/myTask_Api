6a7a30a5292ac8ed4f893aee816027886cf3976c
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2023-03-10 17:13:32 +0000

    delete todo added

--- myShop.Application/Command/myTodo/GetAllMyTodoCommand.cs
+++ myShop.Application/Command/myTodo/GetAllMyTodoCommand.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using MediatR;
+using myShop.Application.Dto.Todo;
+using myShop.Application.Interface;
+using myShop.Domain.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,26 @@ using System.Threading.Tasks;
 
 namespace myShop.Application.Command.myTodo
 {
-    internal class GetAllMyTodoCommand
+    public class GetAllMyTodoCommand: IRequest<List<getTodoDto>>
     {
+    }
+
+    public class GetAllMyTodoCommandHandler : IRequestHandler<GetAllMyTodoCommand, List<getTodoDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IGenericRepository<TblMyTodo> _repository;
+
+        public GetAllMyTodoCommandHandler(IMapper mapper, IGenericRepository<TblMyTodo> repository)
+        {
+            _mapper = mapper;
+               _repository = repository;
+        }
+        public async Task<List<getTodoDto>> Handle(GetAllMyTodoCommand request, CancellationToken cancellationToken)
+        {
+            FormattableString getResponse = $"EXEC [dbo].[spcGetAllMyToDo]";
+            var get = await _repository.GetAll(getResponse);
+            var getData = _mapper.Map<List<getTodoDto>>(get);
+            return getData;
+        }
     }
 }
